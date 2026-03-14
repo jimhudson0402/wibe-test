d7e9e02f9517e99aa1d527e0e4ee3d00af1ec3eb
jimhudson0402 <jimhudson0402@gmail.com>
2026-03-14 22:46:33 +0100

    Extract age input validation into get_age function

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world")
+def get_name():
+    while True:
+        name = input("Как вас зовут? ").strip()
+
+        if name:
+            return name
+
+        print("Вы не ввели имя")
+
+
+def get_age():
+    while True:
+        age = input("Сколько тебе лет? ").strip()
+
+        if age.isdigit():
+            return age
+
+        print("Возраст должен быть числом")
+
+
+name = get_name()
+print(f"Привет, {name}")
+age = get_age()
+print(f"Тебе {age} лет")
