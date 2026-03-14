68fb2109bf5add99b8b0c2c8663580167c098ce8
jimhudson0402 <jimhudson0402@gmail.com>
2026-03-14 22:41:48 +0100

    Extract name input loop into get_name function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+def get_name():
+    while True:
+        name = input("Как вас зовут? ").strip()
+
+        if name:
+            return name
+
+        print("Вы не ввели имя")
+
+
+name = get_name()
+print(f"Привет, {name}")
+age = input("Сколько тебе лет? ").strip()
+print(f"Тебе {age} лет")
