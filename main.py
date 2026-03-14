c430de355f55a5d5d5848f8f22111c9a87cc1112
jimhudson0402 <jimhudson0402@gmail.com>
2026-03-14 23:05:15 +0100

    Wrap CLI flow into main function

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hello world")
+def get_name():
+    while True:
+        name = input("Как вас зовут? ").strip()
+
+        if name:
+            return name
+
+        print("Вы не ввели имя")
+
+
+def get_age():
+    while True:
+        age = input("Сколько тебе лет? ").strip()
+
+        if age.isdigit():
+            return int(age)
+
+        print("Возраст должен быть числом")
+
+
+def main():
+    name = get_name()
+    age = get_age()
+    print(f"Привет, {name}")
+    print(f"Тебе {age} лет")
+    print(f"Через 10 лет тебе будет {age + 10} лет")
+
+
+if __name__ == "__main__":
+    main()
